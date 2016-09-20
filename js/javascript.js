d6e0ce37ded20af96e8adde3dc6d94c040a1e93f
wahlforss <alfred@wahlforss.com>
2016-09-20 19:52:37 +0200

    added button

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,5 @@
-$(document).ready(function() {
-	for(i=1;i <= 256; i++) {
-	$("#grid").append("<div></div>");
-};
 
-});
-$(document).ready(function() {
+function hover() {
 	$("#grid > div").hover(function() {
 			$(this).css("background-color","#ff6161");
 		}, 
@@ -14,5 +9,24 @@ $(document).ready(function() {
 		}
 
 		);
+}
+
+$(document).ready(function() {
+	for(i=1;i <= 256; i++) {
+	$("#grid").append("<div></div>");
+};
+	hover();
+
 });
+function create_grid() {
+	var number = prompt('How many rows and colons?')
+	$('#grid').html('');
+	for(i=1;i<= number*number;i++) {
+		$('#grid').append('<div></div>');
+	};
+	var w = 480/number
+	$('#grid > div').css({'width':w, 'height':w});
+	hover();
+}
+
 
