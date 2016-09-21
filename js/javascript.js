dd17dfb87955b5f022026fda36ec8a35f0bdfb4a
wahlforss <alfred@wahlforss.com>
2016-09-21 12:39:58 +0200

    TRUMP

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,41 @@
-
+var number = 16
 function hover() {
 	$("#grid > div").hover(function() {
-			$(this).css("background-color","#ff6161");
+			// $(this).css("background-color","#ff6161");
+			$(this).addClass('clinton');
+
 		}, 
 		function() {
-			$(this).css("background-color","#ff9393");
+			// $(this).css("background-image", "url(' + images/clinton.jpg + ')");
+			$(this).css('background-image', 'url("' +  images/clinton.jpg + '")');
+
 
 		}
 
 		);
 }
 
-$(document).ready(function() {
-	for(i=1;i <= 256; i++) {
-	$("#grid").append("<div></div>");
-};
-	hover();
-
-});
 function create_grid() {
-	var number = prompt('How many rows and colons?')
 	$('#grid').html('');
 	for(i=1;i<= number*number;i++) {
 		$('#grid').append('<div></div>');
 	};
 	var w = 480/number
 	$('#grid > div').css({'width':w, 'height':w});
+	$('#grid > div').addClass('trump');
 	hover();
 }
+
+$(document).ready(function() {
+	create_grid()
+
+});
+
+function create_grid_button() {
+	number = prompt('How many rows and colons?')
+	create_grid()
+
+}
+
 
 
